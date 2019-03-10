2ccb10a9acf2df96cdaa8c22f5f8253e6bf5fa7b
Ofir <nesher123@gmail.com>
2019-03-10 14:52:08 +0200

    get and post (hard-coded) is working. missing DB

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from bottle import route, run
-import webbrowser
+from bottle import route, run, post, get, request
 
 names = ['Roman', 'Ofir']
- 
-f = open('index.html','w')
  
 message = """<html>
 <body>
@@ -14,8 +11,8 @@ message = """<html>
         </br>
     </div>
 
-    <form method="POST" action="http://localhost:3000/addName"> Name:
-        <input type="text" name="name" value="">
+    <form method="POST" action="/addName"> Name:
+        <input type="text" name="name" >
         <input type="submit" value="Submit">
     </form>
 </body>
@@ -23,19 +20,16 @@ message = """<html>
 
 new_message = message.format(names=names)
 
-# f.write(new_message)
-f.close()
-
-@route('/')
+@get('/')
 def dashboard():
     return new_message
 
-@route('/addName')
-def add():
-    names.append('a')
+@post('/addName')
+def addName():
+    newName = request.forms.get('name')
+    names.append(newName)
+    new_message = message.format(names=names)
+    return new_message
+
 
 run(host='localhost', port=3000)
-
-# #Change path to reflect file location
-# filename = 'index.html'
-# webbrowser.open_new_tab(filename)
